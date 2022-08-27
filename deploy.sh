ec8ef2349b1ddf8e54f392ae5c313a1e42d0eb9e
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-08-27 14:25:47 +0200

    🧪 trying to deploy to github pages

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git init
 git add -A
 git commit -m 'deploy'
 
@@ -19,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:purplehippo911/vueWeatherApp.git main:gh-pages
 
 cd -
