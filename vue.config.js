5c6d6b22381ec3a8822be81b6455df598aba4005
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-08-28 12:26:33 +0200

    added something to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vueWeatherApp/'
-    : '/'
-}
+    : '/',
+    configureWebpack: {
+      plugins: [new MiniCssExtractPlugin()],
+    }
+  };
+  
